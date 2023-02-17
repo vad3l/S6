c05f4e70333605ce033b314a85987ff1cd94ca43
NoirLunaire <117916676+NoirLunaire@users.noreply.github.com>
2023-02-17 21:41:00 +0100

    Je suis un génie

--- testSerial.cc
+++ testSerial.cc
@@ -17,7 +17,7 @@ TEST(Constructor , OBinaryFile) {
 		OBinaryFile fileToWrite = OBinaryFile(path,OBinaryFile::Mode::Truncate);
 
 		//const char bytes[] = { 72, 101, 108, 108, 111 };
-		std::byte toWrite{65};
+		std::byte toWrite{ 'A' };
 		fileToWrite.write(&toWrite,sizeof(std::byte));
 	
 	}
@@ -26,7 +26,18 @@ TEST(Constructor , OBinaryFile) {
 	std::byte toRead{255};
 	std::size_t size = fileToRead.read(&toRead,sizeof(std::byte));
 	EXPECT_EQ(size,sizeof(std::byte));
-
 	std::cout << static_cast<char>(toRead) << std::endl;
+}
 
+TEST(Operator, OBinaryFile) {
+	std::string path((std::string)std::filesystem::temp_directory_path() + "/pmp.bin");
+	{
+		OBinaryFile file = OBinaryFile(path, OBinaryFile::Mode::Truncate);
+		file << (int32_t)5 << 5.f << 'a';
+	}
+	IBinaryFile file = IBinaryFile(path);
+	std::byte reading[4 + 4 + 1];
+	std::size_t size = file.read(reading, sizeof(std::byte) * (4 + 4 + 1));
+	EXPECT_EQ(size, sizeof(std::byte) * (4 + 4 + 1));
+	std::cout << static_cast<char>(reading[8]) << std::endl;
 }
