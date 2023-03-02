30857b98368793bcd1f3d3e4fa3d7271a5670b13
Vad3L <vandrepoleo@gmail.com>
2023-03-02 11:23:25 +0100

    STOP PUT SPACE IN TAB

--- AS/turtle/turtle/turtle-ast.h
+++ AS/turtle/turtle/turtle-ast.h
@@ -6,62 +6,62 @@
 
 // simple commands
 enum ast_cmd {
-  CMD_UP,
-  CMD_DOWN,
-  CMD_RIGHT,
-  CMD_LEFT,
-  CMD_HEADING,
-  CMD_FORWARD,
-  CMD_BACKWARD,
-  CMD_POSITION,
-  CMD_HOME,
-  CMD_COLOR,
-  CMD_PRINT,
+	CMD_UP,
+	CMD_DOWN,
+	CMD_RIGHT,
+	CMD_LEFT,
+	CMD_HEADING,
+	CMD_FORWARD,
+	CMD_BACKWARD,
+	CMD_POSITION,
+	CMD_HOME,
+	CMD_COLOR,
+	CMD_PRINT,
 };
 
 // internal functions
 enum ast_func {
-  FUNC_COS,
-  FUNC_RANDOM,
-  FUNC_SIN,
-  FUNC_SQRT,
-  FUNC_TAN,
+	FUNC_COS,
+	FUNC_RANDOM,
+	FUNC_SIN,
+	FUNC_SQRT,
+	FUNC_TAN,
 };
 
 // kind of a node in the abstract syntax tree
 enum ast_kind {
-  KIND_CMD_SIMPLE,
-  KIND_CMD_REPEAT,
-  KIND_CMD_BLOCK,
-  KIND_CMD_PROC,
-  KIND_CMD_CALL,
-  KIND_CMD_SET,
+	KIND_CMD_SIMPLE,
+	KIND_CMD_REPEAT,
+	KIND_CMD_BLOCK,
+	KIND_CMD_PROC,
+	KIND_CMD_CALL,
+	KIND_CMD_SET,
 
-  KIND_EXPR_FUNC,
-  KIND_EXPR_VALUE,
-  KIND_EXPR_UNOP,
-  KIND_EXPR_BINOP,
-  KIND_EXPR_BLOCK,
-  KIND_EXPR_NAME,
+	KIND_EXPR_FUNC,
+	KIND_EXPR_VALUE,
+	KIND_EXPR_UNOP,
+	KIND_EXPR_BINOP,
+	KIND_EXPR_BLOCK,
+	KIND_EXPR_NAME,
 };
 
 #define AST_CHILDREN_MAX 3
 
 // a node in the abstract syntax tree
 struct ast_node {
-  enum ast_kind kind; // kind of the node
+	enum ast_kind kind; // kind of the node
 
-  union {
-    enum ast_cmd cmd;   // kind == KIND_CMD_SIMPLE
-    double value;       // kind == KIND_EXPR_VALUE, for literals
-    char op;            // kind == KIND_EXPR_BINOP or kind == KIND_EXPR_UNOP, for operators in expressions
-    char *name;         // kind == KIND_EXPR_NAME, the name of procedures and variables
-    enum ast_func func; // kind == KIND_EXPR_FUNC, a function
-  } u;
+	union {
+		enum ast_cmd cmd;	 // kind == KIND_CMD_SIMPLE
+		double value;			 // kind == KIND_EXPR_VALUE, for literals
+		char op;						// kind == KIND_EXPR_BINOP or kind == KIND_EXPR_UNOP, for operators in expressions
+		char *name;				 // kind == KIND_EXPR_NAME, the name of procedures and variables
+		enum ast_func func; // kind == KIND_EXPR_FUNC, a function
+	} u;
 
-  size_t children_count;  // the number of children of the node
-  struct ast_node *children[AST_CHILDREN_MAX];  // the children of the node (arguments of commands, etc)
-  struct ast_node *next;  // the next node in the sequence
+	size_t children_count;	// the number of children of the node
+	struct ast_node *children[AST_CHILDREN_MAX];	// the children of the node (arguments of commands, etc)
+	struct ast_node *next;	// the next node in the sequence
 };
 
 
@@ -72,7 +72,7 @@ struct ast_node *make_expr_value(double value);
 
 // root of the abstract syntax tree
 struct ast {
-  struct ast_node *unit;
+	struct ast_node *unit;
 };
 
 // do not forget to destroy properly! no leaks allowed!
@@ -80,13 +80,13 @@ void ast_destroy(struct ast *self);
 
 // the execution context
 struct context {
-  double x;
-  double y;
-  double angle;
-  bool up;
+	double x;
+	double y;
+	double angle;
+	bool up;
 
-  // TODO: add procedure handling
-  // TODO: add variable handling
+	// TODO: add procedure handling
+	// TODO: add variable handling
 };
 
 // create an initial context
--- AS/turtle/turtle/turtle.c
+++ AS/turtle/turtle/turtle.c
@@ -7,26 +7,26 @@
 #include "turtle-parser.h"
 
 int main() {
-  srand(time(NULL));
+	srand(time(NULL));
 
-  struct ast root;
-  int ret = yyparse(&root);
+	struct ast root;
+	int ret = yyparse(&root);
 
-  if (ret != 0) {
-    return ret;
-  }
+	if (ret != 0) {
+		return ret;
+	}
 
-  yylex_destroy();
+	yylex_destroy();
 
-  assert(root.unit);
+	assert(root.unit);
 
-  struct context ctx;
-  context_create(&ctx);
+	struct context ctx;
+	context_create(&ctx);
 
-  ast_eval(&root, &ctx);
-//   ast_print(&root);
+	ast_eval(&root, &ctx);
+//	 ast_print(&root);
 
-  ast_destroy(&root);
+	ast_destroy(&root);
 
-  return ret;
+	return ret;
 }
