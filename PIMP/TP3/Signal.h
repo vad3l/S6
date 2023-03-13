952a40328ace28c8a97f85a1d6ad4d5e447aec6d
Vad3L <vandrepoleo@gmail.com>
2023-03-13 11:44:56 +0100

    tp 3

--- PIMP/TP3/Signal.h
+++ PIMP/TP3/Signal.h
@@ -3,73 +3,73 @@
 
 namespace sig {
 
-  class DiscardCombiner {
-  public:
-    using result_type = /* implementation defined */;
+	class DiscardCombiner {
+	public:
+		using result_type = /* implementation defined */;
 
-    template<typename U>
-    void combine(/* implementation defined */ item) {
-      // implementation defined
-    }
+		template<typename U>
+		void combine(/* implementation defined */ item) {
+			// implementation defined
+		}
 
-    result_type result() {
-      // implementation defined
-    }
-  };
+		result_type result() {
+			// implementation defined
+		}
+	};
 
-  template<typename T>
-  class LastCombiner {
-  public:
-    using result_type = /* implementation defined */;
+	template<typename T>
+	class LastCombiner {
+	public:
+		using result_type = /* implementation defined */;
 
-    template<typename U>
-    void combine(/* implementation defined */ item) {
-      // implementation defined
-    }
+		template<typename U>
+		void combine(/* implementation defined */ item) {
+			// implementation defined
+		}
 
-    result_type result() {
-      // implementation defined
-    }
-  };
+		result_type result() {
+			// implementation defined
+		}
+	};
 
-  template<typename T>
-  class VectorCombiner {
-  public:
-    using result_type = /* implementation defined */;
+	template<typename T>
+	class VectorCombiner {
+	public:
+		using result_type = /* implementation defined */;
 
-    template<typename U>
-    void combine(/* implementation defined */ item) {
-      // implementation defined
-    }
+		template<typename U>
+		void combine(/* implementation defined */ item) {
+			// implementation defined
+		}
 
-    result_type result() {
-      // implementation defined
-    }
-  };
+		result_type result() {
+			// implementation defined
+		}
+	};
 
-  template<typename Signature, typename Combiner = DiscardCombiner>
-  class Signal {
-  public:
-    using combiner_type = /* implementation defined */;
+	template<typename Signature, typename Combiner = DiscardCombiner>
+	class Signal {
+	public:
+		using combiner_type = /* implementation defined */;
 
-    using result_type = /* implementation defined */;
+		using result_type = /* implementation defined */;
 
-    Signal(Combiner combiner = Combiner()) {
-      // implementation defined
-    }
+		Signal(Combiner combiner = Combiner()) {
+			// implementation defined
+		}
 
-    std::size_t connectSlot(std::function<Signature> callback) {
-      // implementation defined
-    }
+		std::size_t connectSlot(std::function<Signature> callback) {
+			// implementation defined
+		}
 
-    void disconnectSlot(std::size_t id) {
-      // implementation defined
-    }
+		void disconnectSlot(std::size_t id) {
+			// implementation defined
+		}
 
-    result_type emitSignal(Args... args) {
-      // implementation defined
-    }
-  };
+		result_type emitSignal(Args... args) {
+			// implementation defined
+		}
+	};
 
 }
 
