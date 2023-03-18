69880323927c2dd759d9064ee0f53d8a202583e9
NoirLunaire <117916676+NoirLunaire@users.noreply.github.com>
2023-03-18 20:02:37 +0100

    aucune idée de ce que je fais

--- PIMP/TP3/Signal.h
+++ PIMP/TP3/Signal.h
@@ -1,73 +1,90 @@
 #ifndef SIGNAL_H
 #define SIGNAL_H
 
+#include <vector>
+#include <functional>
+#include <type_traits>
+
 namespace sig {
 
 	class DiscardCombiner {
 	public:
-		using result_type = /* implementation defined */;
+		using result_type = void;
 
 		template<typename U>
-		void combine(/* implementation defined */ item) {
-			// implementation defined
+		void combine(U&& item) {
 		}
 
 		result_type result() {
-			// implementation defined
 		}
 	};
 
 	template<typename T>
 	class LastCombiner {
 	public:
-		using result_type = /* implementation defined */;
+		using result_type = T;
+		T last;
+		
 
 		template<typename U>
-		void combine(/* implementation defined */ item) {
-			// implementation defined
+		void combine(U&& item) {
+			last = item;
 		}
 
 		result_type result() {
-			// implementation defined
+			return last;
 		}
 	};
 
 	template<typename T>
 	class VectorCombiner {
 	public:
-		using result_type = /* implementation defined */;
+		using result_type = std::vector<T>;
+		std::vector<T> all;
 
 		template<typename U>
-		void combine(/* implementation defined */ item) {
-			// implementation defined
+		void combine(U&& item) {
+			all.push_back(item);
 		}
 
 		result_type result() {
-			// implementation defined
+			return all;
 		}
 	};
 
 	template<typename Signature, typename Combiner = DiscardCombiner>
 	class Signal {
 	public:
-		using combiner_type = /* implementation defined */;
+		using combiner_type = Combiner;
+		using result_type = typename std::function<Signature>::result_type;
 
-		using result_type = /* implementation defined */;
-
-		Signal(Combiner combiner = Combiner()) {
-			// implementation defined
+		Combiner combiner;
+		std::vector<std::function<Signature>> functions;
+		Signal(Combiner c = Combiner()) {
+			combiner = c;
 		}
 
 		std::size_t connectSlot(std::function<Signature> callback) {
-			// implementation defined
+			functions.push_back(callback);
+			return functions.size() - 1;
 		}
 
 		void disconnectSlot(std::size_t id) {
-			// implementation defined
+			functions.erase(functions.begin() + id);
 		}
 
+		template<typename... Args>
 		result_type emitSignal(Args... args) {
-			// implementation defined
+			for (size_t i = 0; i < functions.size(); i++) {
+				if constexpr (std::is_same<result_type, void>::value) {
+					functions[i](args...);
+				} else {
+					combiner.combine(functions[i](args...));
+				}
+			}
+			if (std::is_same<result_type, void>::value) {
+				return combiner.result();
+			}
 		}
 	};
 
--- PIMP/TP3/testSignal.cc
+++ PIMP/TP3/testSignal.cc
@@ -2,6 +2,15 @@
 
 #include <gtest/gtest.h>
 
+TEST (Cours, Cours) {
+	sig::Signal<void(int)> s;
+	int res = 0;
+	s.connectSlot([&res](int x) { res += x; });
+	s.connectSlot([&res](int x) { res += x; });
+	s.emitSignal(1);
+	EXPECT_EQ(res, 2);
+}
+
 int main(int argc, char* argv[]) {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
